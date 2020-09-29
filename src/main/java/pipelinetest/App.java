7dd7a7570939df8f87b0152e907be5fedcfa4533
Mostafa <m.torbon@gmx.de>
2020-09-29 15:18:57 +0200

    Initialisierung

---
+++ src/main/java/pipelinetest/App.java
@@ -0,0 +1,13 @@
+package pipelinetest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
